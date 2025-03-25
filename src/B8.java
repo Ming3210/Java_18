3ea2131166988df554ceb61143f95cdb5d020a9e
unknown <vminh0227@gmail.com>
2025-03-25 09:30:57 +0700

    add

--- src/B8.java
+++ src/B8.java
@@ -1,28 +1,47 @@
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class B8 {
     public static void main(String[] args) {
-        Map<Integer, String> num = new HashMap<>();
-        num.put(1,"I");
-        num.put(2,"II");
-        num.put(3,"III");
-        num.put(4,"IV");
-        num.put(5,"V");
-        num.put(6,"VI");
-        num.put(7,"VII");
-        num.put(8,"VIII");
-        num.put(9,"IX");
-        num.put(10,"X");
-        num.put(11,"XI");
-        num.put(12,"XII");
-        num.put(13,"XIII");
-        System.out.println("Nhap so: ");
         Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        if (n >= 1 && n <= 13) {
-            System.out.println(num.get(n));
+        int n;
+
+        while (true) {
+            System.out.print("Nhập số nguyên dương N (1 - 3999): ");
+            n = scanner.nextInt();
+            if (n > 0 && n <= 3999) break;
+            System.out.println("Vui lòng nhập số từ 1 đến 3999!");
         }
+
+        String romanNumeral = convertToRoman(n);
+        System.out.println("Số La Mã tương ứng: " + romanNumeral);
+
+        scanner.close();
+    }
+
+    public static String convertToRoman(int num) {
+        LinkedHashMap<Integer, String> romanMap = new LinkedHashMap<>();
+        romanMap.put(1000, "M");
+        romanMap.put(900, "CM");
+        romanMap.put(500, "D");
+        romanMap.put(400, "CD");
+        romanMap.put(100, "C");
+        romanMap.put(90, "XC");
+        romanMap.put(50, "L");
+        romanMap.put(40, "XL");
+        romanMap.put(10, "X");
+        romanMap.put(9, "IX");
+        romanMap.put(5, "V");
+        romanMap.put(4, "IV");
+        romanMap.put(1, "I");
+
+        StringBuilder roman = new StringBuilder();
+
+        for (Map.Entry<Integer, String> entry : romanMap.entrySet()) {
+            while (num >= entry.getKey()) {
+                roman.append(entry.getValue());
+                num -= entry.getKey();
+            }
+        }
+        return roman.toString();
     }
 }
